be289dd81a4d66026f4cdead38926a2f321fa2e6
Adam M. Jacobs <ajacobs@pa.msu.edu>
2020-03-05 02:17:34 -0500

    draft playground template

---
+++ awk/awk_playground.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# This bash shell script serves as a basic tutorial of and playground for
+# exploring `awk`.  By default, when you pull this repository, all commands are
+# commented out.  However, you can uncomment commands to execute this script.
+# Or, you can simply read along and execute the commands yourself in a terminal
+# in this directory.
